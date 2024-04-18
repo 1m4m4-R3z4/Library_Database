fbd23ab713378d8292f0e91b3b31aba9a791f6cf
Sanakhn <149523045+Sanakhn@users.noreply.github.com>
2024-04-18 15:16:52 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -2,7 +2,7 @@ package com.CSCI300;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.util.Scanner;
 class Database {
     public static Connection connection;
 
@@ -14,4 +14,12 @@ class Database {
             System.out.println(e);
         }
     }
+    public static void setupClosingDBConnection() {
+        Runtime.getRuntime().addShutdownHook (new Thread (new Runnable() {
+            public void run() {
+                try { Database.connection.close(); 
+            } catch (SQLException e) {e.printStackTrace(); }
+        }
+    }, "Shutdown-thread"));
 }
+    
