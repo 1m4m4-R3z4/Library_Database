851eac50fbeaa0c4910e673fc9948adf8c4fab56
Dylan Edwards <106411648+voonichu@users.noreply.github.com>
2024-04-22 14:34:34 -0400

    Update Main.java to include add books function
    Needs sql jar file to be imported into the build path, tried to add loop to function but every loop past 1 gives me an error. Also requires an author_id and publisher_id to already exist within their tables and use existing values otherwise an error occurs.

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
 package com.CSCI300;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    	Database.connect();
+      	setupClosingDBConnection();
+    	addBooks();
+    	
     }
 
     public static void setupClosingDBConnection() {
@@ -16,5 +20,38 @@ public class Main {
             }
         }, "Shutdown-thread"));
     }
+    
+    public static void addBooks() {
+		try {
+			Scanner input = new Scanner(System.in);
+			System.out.println("What is the Book ID");
+			int bookid = input.nextInt();
+			System.out.println("What is the Author ID");
+			int authorid = input.nextInt();
+			System.out.println("What is the Book's Title");
+			input.nextLine();
+			String booktitle = input.nextLine();
+			System.out.println("What is the Genre?");
+			String genre = input.nextLine();
+			System.out.println("What is the Publisher ID");
+			int publisherid = input.nextInt();
+			System.out.println("Is the book Paperback or Hardcover?");
+			input.nextLine();
+			String book_type = input.nextLine();
+			Connection connection = Database.connection;
+			String query = "INSERT INTO Books VALUES (?, ?, ?, ?, ?, ?)";
+			PreparedStatement stm = connection.prepareStatement(query);
+			
+			stm.setInt(1, bookid);
+			stm.setInt(2, authorid);
+			stm.setString(3, booktitle);
+			stm.setString(4, genre);
+			stm.setInt(5, publisherid);
+			stm.setString(6, book_type);
+			stm.executeUpdate();
+			input.close();
+		} catch (Exception e) {
+			System.out.println(e);
+    	}
+    }
 }
-
